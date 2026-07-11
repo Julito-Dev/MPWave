b1e027c5d8344156d8b17d8d33ca360bfde24b6b
Julito-Dev <julianpaez180@gmail.com>
2026-07-11 13:22:16 -0500

    feat: the ui nows ask directory before make a conversion

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from tkinter import filedialog, messagebox
 import os
+import threading
 from converter import convert
 
 
@@ -13,15 +14,26 @@ class ConvertApp:
         self.root = root
         self.root.title("Conversor MP3 <-> WAV")
         self.file_route = None
+        self.output_folder = None
         self._buildUI()
         
         
     def _buildUI(self):
         self.label_file = ctk.CTkLabel(self.root, text="No file Selected")
-        self.label_file.pack(pady = 20)
+        self.label_file.pack(pady = 10)
         
         ctk.CTkButton(self.root, text="Select File", command=self.select_file).pack(pady=5)
-        ctk.CTkButton(self.root, text="Convert", command=self.convert_file).pack(pady=5)
+        self.label_folder = ctk.CTkLabel(self.root, text="Destination: same as file")
+        self.label_folder.pack(pady=10)
+        
+        
+        ctk.CTkButton(self.root, text="Select destination Folder", command=self.select_folder).pack(pady=5)
+        
+        self.convert_button = ctk.CTkButton(self.root, text="Convert", command=self.convert_file)
+        self.convert_button.pack(pady=10)
+
+        self.progress_bar = ctk.CTkProgressBar(self.root, mode="indeterminate")
+        
         
         
     def select_file(self):
@@ -29,6 +41,13 @@ class ConvertApp:
         if route:
             self.file_route = route
             self.label_file.configure(text=os.path.basename(route))
+            
+    def select_folder(self):
+        folder = filedialog.askdirectory()
+        if folder:
+            self.output_folder = folder
+            self.label_folder.configure(text=f"Destination: {folder}")
+            
             
     def convert_file(self):
         if not self.file_route:
